c9d358747e0a402ca195eb4c4012f9a3e8b10b1c
Sunny S <3030712@gmail.com>
2014-09-11 03:32:46 -0700

    updated logstream name to loglevel

--- include/logger.hpp
+++ include/logger.hpp
@@ -5,11 +5,13 @@
 #ifndef STREAMLOG_LOGGER_HPP
 #define STREAMLOG_LOGGER_HP
 
+#include "loglevel.hpp"
+
 namespace streamlog {
 
 template <typename CharT, typename Traits>
 streamlog::bits::logstream_stream<CharT,Traits> &
-logger(logstream &, std::basic_ostream<CharT,Traits> &);
+logger(loglevel &, std::basic_ostream<CharT,Traits> &);
 
 }/* streamlog */
 #include "bits/logging.tcc"
